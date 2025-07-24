8ac345868b08552515974392f89cd026bb1d83b3
Owen Brooks <owen.h.brooks@gmail.com>
2025-07-24 20:56:12 +1000

    Add tailwind for styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,22 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://owenbrooks.github.io',
-    integrations: [mdx(), sitemap()],
-    markdown: {
-        shikiConfig: {
-            // theme: 'gruvbox-dark-medium',
-            // theme: 'laserwave',
-            theme: 'one-dark-pro',
-        },
-    },
+  site: 'https://owenbrooks.github.io',
+  integrations: [mdx(), sitemap()],
+
+  markdown: {
+      shikiConfig: {
+          // theme: 'gruvbox-dark-medium',
+          // theme: 'laserwave',
+          theme: 'one-dark-pro',
+      },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });

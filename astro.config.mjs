b95a6d4a68d85f282469759487e1406b5d63abeb
Owen Brooks <owen.h.brooks@gmail.com>
2025-07-26 15:55:52 +1000

    Configure domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://owenbrooks.github.io',
+  site: 'https://owenbrooks.org',
   integrations: [mdx(), sitemap()],
 
   markdown: {
